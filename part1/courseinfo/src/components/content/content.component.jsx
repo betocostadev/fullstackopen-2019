eeb02919a1f0a00ce7d2a1626758a9d95fc75544
betocostadev <robertomarx@gmail.com>
2019-09-12 10:58:09 -0300

    refactored some files

--- part1/courseinfo/src/components/content/content.component.jsx
+++ part1/courseinfo/src/components/content/content.component.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import './content.styles.scss';
 
-const Content = ({...props}) => (
-  <section className="content">
-    <Part part={props.part1} exercise={props.exercises1}/>
-    <Part part={props.part2} exercise={props.exercises2}/>
-    <Part part={props.part3} exercise={props.exercises3}/>
-  </section>
-)
+const Content = () => {
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
-const Part = ({part, exercise}) => (
-  <p>{part} {exercise}</p>
+  console.log(parts[0])
+  return(
+    <section className="content">
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </section>
+  )
+}
+
+// const Content = (props) => (
+//   <section className="content">
+//     <Part part={props.name} exercise={props.exercise} />
+//     <Part part={props.name} exercise={props.exercise} />
+//     <Part part={props.name} exercise={props.exercise} />
+//   </section>
+// )
+
+const Part = (props) => (
+  <p>{props.name} {props.exercises}</p>
 )
 
 export default Content;
--- part1/courseinfo/src/components/total/total.component.jsx
+++ part1/courseinfo/src/components/total/total.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import './header.styles.scss';
 
 const Total = (props) => (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
 )
 
 export default Total;
