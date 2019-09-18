bf0772996e54cf86b8561fbf14cf3d2ca8b04682
betocostadev <robertomarx@gmail.com>
2019-09-18 11:52:31 -0300

    part 1 c done

--- part1/src/components/hello.component.jsx
+++ part1/src/components/hello.component.jsx
@@ -10,5 +10,4 @@ const Hello = ({name, age}) => {
   )
 }
 
-
 export default Hello;
--- part1/src/index.js
+++ part1/src/index.js
@@ -2,23 +2,7 @@ import React, {useState} from 'react'; // Adding useState for the state hook
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Hello from './components/hello.component'
-
-// const App = () => {
-//   const now = new Date()
-//   const a = 10
-//   const b = 20
-
-//   return (
-//     <div>
-//       <p>Hello World, it is {now.toString()}</p>
-//       <p>
-//         {a} plus {b} is {a + b}
-//       </p>
-//     </div>
-//   )
-// }
-
+import Hello from './components/hello.component';
 
 // Footer component test
 const Footer = (props) => {
@@ -31,14 +15,32 @@ const Footer = (props) => {
   )
 }
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
 
 const App = (props) => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => {
-    setCounter(counter + 1)
-  }, 1000);
-  console.log('rendering...', counter)
+  // setTimeout(() => {
+  //   setCounter(counter + 1)
+  // }, 1000);
+  // console.log('rendering...', counter)
+
+  // Using a function that will call another function.
+  // This will avoid the button call to break the app, as it will not be called on load forever
+  // const setToValue = (value) => {
+  //   return () => {
+  //     setCounter(value);
+  //   }
+  // }
+  // Same thing, but now, using double arrow functions:
+  // const setToValue = (value) => () => setCounter(value);
+  const setToValue = (value) => setCounter(value);
+
   const name = 'Beto'
   const age = 33
   const courseLink1 = "https://fullstackopen.com/en/part1/introduction_to_react#component"
@@ -47,7 +49,10 @@ const App = (props) => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={1 + 1} />
       <Hello name={name} age={age} />
-      <p>Counter is: {counter}</p>
+      <Display counter={counter} />
+      <Button onClick={() => setToValue(counter + 1)} text='plus' />
+      <Button onClick={() => setToValue(counter - 1)} text='minus' />
+      <Button onClick={() => setToValue(0)} text='zero' />
       <Footer link={courseLink1} />
     </div>
   )
