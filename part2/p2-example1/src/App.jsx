3a1b41d8da360d2b42c4a4ddcb21a9099ee46a06
betocostadev <robertomarx@gmail.com>
2019-10-04 16:03:13 -0300

    change important note to be set by the user

--- part2/p2-example1/src/App.jsx
+++ part2/p2-example1/src/App.jsx
@@ -6,6 +6,7 @@ import Note from './components/note/note.component'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
+  const [importantNote, setImportantNote] = useState('checked');
   const [showAll, setShowAll] = useState(true)
 
   const notesToShow = showAll
@@ -21,19 +22,26 @@ const App = (props) => {
   const addNote = (event) => {
     event.preventDefault()
     // console.log('button clicked', event.target)
+    //   important: Math.random() > 0.5,
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() > 0.5,
+      important: importantNote,
       id: notes.length + 1
     }
     setNotes(notes.concat(noteObject))
     setNewNote('')
+    setImportantNote(false)
   }
 
   const handleNoteChange = (event) => {
     // console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+
+  const handleImportance = (event) => {
+    console.log(event.target.checked)
+    setImportantNote(event.target.checked)
   }
 
   return (
@@ -53,6 +61,7 @@ const App = (props) => {
           value={newNote}
           onChange={handleNoteChange}/>
         <button type='submit'>save</button>
+        <input type='checkbox' checked={importantNote} onChange={handleImportance} />Important?
       </form>
     </div>
   )
