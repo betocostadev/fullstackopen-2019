0d3c7a4ce08fdfb4070e46ba0856622c381da8fa
betocostadev <robertomarx@gmail.com>
2019-10-09 15:44:20 -0300

    phonebook Step6 - added json db, added axios, getting data from the db

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
+import axios from 'axios'
 
 // Components
 import Search from './components/search/search.component'
@@ -7,16 +9,23 @@ import AddNewPerson from './components/add-new-person/add-new-person.component'
 import Numbers from './components/numbers/numbers.component'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Branlas Lacenon', number: '12-43-234345', id: 4 }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ searchName, setSearchName] = useState('')
   const [ showAll, setShowAll ] = useState(true)
+
+  // useEffect to run axios and fetch data from db.json (json server at localhost://3001)
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const personsToShow = showAll
   ? persons
